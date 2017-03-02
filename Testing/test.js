941726afc499a897a29076451cc0c6dc380ae1f5
smarthutza <smarthutza@yahoo.com>
2017-03-02 12:30:34 +0000

    Wrote a few very simple tests

---
+++ Testing/test.js
@@ -0,0 +1,14 @@
+QUnit.test("if input equals minutes, element(minutes).innerHTML should equal 00", function(assert){
+assert.equal(element("minutes").innerHTML, "00")
+});
+QUnit.test("if input equals seconds, element(seconds).innerHTML should equal 00", function(assert){
+assert.equal(element("seconds").innerHTML, "00")
+});
+QUnit.test("if input equals hours, element(hours).innerHTML should equal 00", function(assert){
+assert.equal(element("hours").innerHTML, "00")
+});
+/*
+QUnit.test("if input equals miliseconds, element(miliseconds).innerHTML should equal 00", function(assert){
+assert.equal(element("miliseconds").innerHTML, "00")
+});
+*/
