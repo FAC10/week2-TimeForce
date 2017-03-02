c4537dbfa0a31838772e2298037ba3b6dabcfb95
Samatar Axmed <Samatar_267@hotmail.com>
2017-03-02 12:24:36 +0000

    Created working timer function I have created a working timer function which updates accordingly. E.g. when miliseconds reaches 99, seconds starts incrementing and so on. I have also added the html elements in the index file to the QUnit test file, because we kept running into errors when the QUnit test file ran as the script file tried accessing DOM elements not available in the QUnit HTML file.

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
+  function element(id){return document.getElementById(id)};
+    var milisecondsUnit = 00;
+    var secondsIncrement = 00;
+    var minutesIncrement = 00;
+    var hoursIncrement = 00;
 
-var miliseconds = document.getElementById("miliseconds");
-var milisecondsTime = 00;
+function milisecondsIncrement(){milisecondsUnit++;return;}
 
-function startTimer(){
-    milisecondsTime+=1;
-    return milisecondsTime;
-}
+  var timer =   setInterval(function() {
+        //Miliseconds;
+        milisecondsIncrement();
+        element("miliseconds").innerHTML = "0" + milisecondsUnit;
+        if (milisecondsUnit > 9) {
+            element("miliseconds").innerHTML = milisecondsUnit;
+        }
+        if (milisecondsUnit > 99) {
+            milisecondsUnit = 0;
+            element("miliseconds").innerHTML = "0" + milisecondsUnit;
+
+            secondsIncrement++;
+          element("seconds").innerHTML = "0" + secondsIncrement;
+        };
+
+        if (secondsIncrement > 9) {
+            element("seconds").innerHTML = secondsIncrement
+        };
+        if (secondsIncrement > 59) {
+            secondsIncrement = 0;
+            element("seconds").innerHTML = "0" + secondsIncrement;
+            minutesIncrement++;
+            element("minutes").innerHTML = "0" + minutesIncrement;
+        }
+
+        if (minutesIncrement > 9) {
+            element("minutes").innerHTML = minutesIncrement
+        };
+        if (minutesIncrement > 59) {
+            minutesIncrement = 0;
+            element("minutes").innerHTML = "0" + minutesIncrement;
+            hoursIncrement++;
+            element("hours").innerHTML = "0" + hoursIncrement;
+        }
+
+
+    }, 10);
